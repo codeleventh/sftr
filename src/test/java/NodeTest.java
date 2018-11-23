c68b81a11098db4e2a028a5c5aa1f053bf4f5775
nvsky <boredomsoul@yandex.ru>
2018-11-24 01:30:24 +0700

    + file I/O

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -7,7 +7,7 @@ class NodeTest {
     @Test
     public void flippingTest() {
         assertEquals(Node.flipTree("a"), "a");
-        assertEquals(Node.flipTree("aaa"), "aaa");
+        assertEquals(Node.flipTree("a(b)"), "a(b)");
         assertEquals(Node.flipTree("aaa(bbb,ccc)"), "aaa(ccc,bbb)");
         assertEquals(Node.flipTree("aaa(bbb(sdf(1,2),ooo(4,5,fgh(6))),456)"), "aaa(456,bbb(ooo(fgh(6),5,4),sdf(2,1)))");
         assertEquals(Node.flipTree("n(o,o,n,ra(t,t t))"), "n(ra(t t,t),n,o,o)");
@@ -16,6 +16,7 @@ class NodeTest {
         assertEquals(Node.flipTree("()"), "ERROR");
         assertEquals(Node.flipTree("a()"), "ERROR");
         assertEquals(Node.flipTree("a("), "ERROR");
+        assertEquals(Node.flipTree("a(bb)zzz"), "ERROR");
         assertEquals(Node.flipTree("a(b(x,,z))"), "ERROR");
         assertEquals(Node.flipTree("a(b,c((d)"), "ERROR");
         assertEquals(Node.flipTree("a(c),b(d)"), "ERROR");
