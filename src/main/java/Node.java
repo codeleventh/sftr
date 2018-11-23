eb57b51fe8d5c88e6b05f791a2e126cd377ed01e
nvsky <boredomsoul@yandex.ru>
2018-11-24 01:13:37 +0700

    refactoring & test

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -3,87 +3,78 @@ import java.util.List;
 
 public class Node {
 
-    String name = "";
-    List<Node> children = new ArrayList<Node>();
+    public String name = "";
+    public List<Node> children = new ArrayList<Node>();
 
     private static String s;
     private static Integer pos;
     private static Integer depth;
 
-    static Node build(String s) throws RuntimeException {
-        System.out.println(s);
-        Node.s = s;
-        pos = depth = 0;
-        return parse();
-    }
-
-    static Node parse() throws RuntimeException {
+    private static Node parse() throws RuntimeException {
         Node node = new Node();
         while (s.length() > pos) {
-            switch (s.charAt(pos)) {
-                case ' ':
-                    pos++;
-                    break;
+            switch (s.charAt(pos++)) {
                 case '(':
-                    pos++;
+                    checkName(node);
                     depth++;
                     node.children.add(Node.parse());
-                    // пропуск всего до )
                     break;
                 case ')':
-                    pos++;
+                    checkName(node);
                     depth--;
                     return node;
                 case ',':
-                    pos++;
+                    checkName(node);
                     node.children.add(Node.parse());
                     break;
-                //                    }
-                //                break;
                 default:
-                    node.name += s.charAt(pos);
-                    pos++;
-                    if (pos < s.length()) {
-                        char a = s.charAt(pos);
-                        if (a == ',' || a == ')') { // :>
-                            return node;
-                        }
-
+                    node.name += s.charAt(pos - 1);
+                    if (pos < s.length() && (s.charAt(pos) == ',' || s.charAt(pos) == ')')) { // :>
+                        return node;
                     }
+                    break;
             }
+
         }
+        checkName(node);
         return node;
+    }
+
+    private static void checkName(Node node) {
+        if(node.name == "")
+            throw new RuntimeException();
+    }
+
+    public static String flipTree(String s) {
+        Node.s = s;
+        pos = depth = 0;
+        Node root;
+        try {
+            root = parse();
+            if (depth != 0 || pos < s.length() - 1) {
+                throw new RuntimeException();
+            }
+        } catch (RuntimeException e) {
+            return "ERROR";
+        }
+        return root.toString();
     }
 
     @Override
     public String toString() {
-        // R to L
         String s = name;
         if (children.size() > 0) {
             s += "(";
-            for (Node n : children) {
-                s += n.toString() + ",";
+            for (int i = children.size() - 1; i >= 0; i--) {
+                s += children.get(i).toString() + ",";
             }
-            s = s.substring(0, s.length() - 1);
-            s += ")";
+            s = s.substring(0, s.length() - 1) + ")";
         }
         return s;
     }
 
-    public static void main(String... s) {
-        System.out.println(Node.build("aaa(bbb,ccc)"));
-        System.out.println(Node.build("aaa(bbb,ccc(dda))"));
-        System.out.println(Node.build("test"));
-        System.out.println(Node.build("aaa(bbb(sdf(1,2),ooo(4,5,fgh(6))),456)"));
-        System.out.println(Node.build("aaa(456,bbb(ooo(fgh(6),5,4),sdf(2,1)))"));
-    }
+    public static void main(String... s) {}
 }
 
 // TODO:
-// test!
 // file IO
-// назовки класс каким-нибудь NodeBuilder
-// строку-то можешь и не передавать
-
-// постпроверка на то, что вся строка съедена, глубина ноль
-// и лови экцепшны
